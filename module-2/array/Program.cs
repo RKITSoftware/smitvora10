0b76855426841c786f192742495c005868a6733d
smitvora <smitvora10@gmail.com>
2021-09-21 10:39:08 +0530

    array

---
+++ module-2/array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };//creating and initializing array  
+            int sum = 0;
+            //traversing array  
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Total sum = {0}", sum);
+
+        }
+    }
+}
