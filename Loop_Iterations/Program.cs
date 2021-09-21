6c685e94ca80dbf111aab488fb1a47c93c300e9e
smitvora <smitvora10@gmail.com>
2021-09-21 10:07:00 +0530

    module_2-while_loop

---
+++ Loop_Iterations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace while_loop
+{
+    class whileloop
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+        }
+    }
+}
