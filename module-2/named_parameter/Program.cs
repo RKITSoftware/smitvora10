f272813a3079d42617601ac02c07099b31212831
smitvora <smitvora10@gmail.com>
2021-09-21 11:20:33 +0530

    different types of parameters in method

---
+++ module-2/named_parameter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace named_parameter
+{
+	public class Operation
+	{
+		// operating contain three parameters
+		public static void operating(int a, int b, int c)
+		{
+			int result = a + b * c;
+			Console.WriteLine("Final result is: " + result);
+		}
+
+		// Main Method
+		static public void Main()
+		{
+			// calling the static method with named
+			// parameters without any order
+			operating(b: 6, a: 2, c: -2);
+
+		}
+	}
+	//result - Final result is: -10
+
+}
---
+++ module-2/optional_or_default_type_parameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace optional_or_default_type_parameter
+{
+	public class Operation
+	{
+		// operating contain three parameters
+		public static void operating(int a, int b, int c = 2)
+		{
+			int result = a + b * c;
+			Console.WriteLine("Final result is: " + result);
+		}
+
+		// Main Method
+		static public void Main()
+		{
+			// calling the static method with named
+			// parameters without any order
+			operating(6, 2);
+
+		}
+	}
+	//result - Final result is: 10
+	//As default value of c is taken as 2
+
+}
