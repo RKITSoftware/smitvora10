4af309a2f8e67a8daf31c88a8883b9334d0ec9d1
smitvora <smitvora10@gmail.com>
2021-10-19 22:26:52 +0530

    Module-2_Commit
    Module-2_Commit

--- module-2/properties_of_Datetime/Program.cs
+++ module-2/properties_of_Datetime/Program.cs
@@ -7,33 +7,78 @@ namespace properties_of_Datetime
         static void Main(string[] args)
         {
             DateTime DateTimeProperty = new DateTime(2021, 9, 17, 7, 10, 24);
-            Console.WriteLine("Day:{0}", DateTimeProperty.Day);
-            Console.WriteLine("Month:{0}", DateTimeProperty.Month);
-            Console.WriteLine("Year:{0}", DateTimeProperty.Year);
-            Console.WriteLine("Hour:{0}", DateTimeProperty.Hour);
-            Console.WriteLine("Minute:{0}", DateTimeProperty.Minute);
-            Console.WriteLine("Second:{0}", DateTimeProperty.Second);
-            Console.WriteLine("Millisecond:{0}", DateTimeProperty.Millisecond);
-            Console.WriteLine("Day of Week:{0}", DateTimeProperty.DayOfWeek);
-            Console.WriteLine("Day of Year: {0}", DateTimeProperty.DayOfYear);
-            Console.WriteLine("Time of Day:{0}", DateTimeProperty.TimeOfDay);
+            Console.WriteLine("Day:{0}", DateTimeProperty.Day);//determines day
+            Console.WriteLine("Month:{0}", DateTimeProperty.Month);//determines month
+            Console.WriteLine("Year:{0}", DateTimeProperty.Year);//determines year
+            Console.WriteLine("Hour:{0}", DateTimeProperty.Hour);//determines hours
+            Console.WriteLine("Minute:{0}", DateTimeProperty.Minute);//determines minutes
+            Console.WriteLine("Second:{0}", DateTimeProperty.Second);//determines seconds
+            Console.WriteLine("Millisecond:{0}", DateTimeProperty.Millisecond);//determines milliseconds
+            Console.WriteLine("Day of Week:{0}", DateTimeProperty.DayOfWeek);//determines which day of the week that particular day is
+            Console.WriteLine("Day of Year: {0}", DateTimeProperty.DayOfYear);//determines which no. of day of the year that particular day is
+            Console.WriteLine("Time of Day:{0}", DateTimeProperty.TimeOfDay);//determines time of the day
+
+
+            int days = DateTime.DaysInMonth(2022,2);
+            Console.WriteLine("Days in February of 2022 : "+days);//determines the days in 2nd month of 2022
+            //Days in February of 2022 : 28
+
+            Console.WriteLine( "Is 2022 a leap year: "+ DateTime.IsLeapYear(2022));//determines whether 2022 is leap year or not
+            //Is 2022 a leap year: False
+
+            //Use of timespan function to add time to a date
+            DateTime objdate = new DateTime(2021, 12, 31);
+            TimeSpan objtimespan = new TimeSpan(25, 20, 20);
+            DateTime newDate = objdate.Add(objtimespan);
+            Console.WriteLine("New Date is : "+newDate);//1/1/2020 1:20:20 AM
+            //New Date is : 01-01-2022 01:20:20
+
+            //Use of timespan function to subtract two dates
+            DateTime objdate1 = new DateTime(2021, 12, 31);
+            DateTime objdate2 = new DateTime(2021, 12, 29);
+            TimeSpan objtimespan1 = objdate1.Subtract(objdate2);
+            Console.WriteLine("Difference : "+objtimespan1);
+            //Difference : 2.00:00:00
+
+            //Difference with compare method
+            int objdiff = DateTime.Compare(objdate1, objdate2);
+
+            if (objdiff > 0)
+                Console.WriteLine("First date is greater");
+            else if (objdiff < 0)
+                Console.WriteLine("Second date is greater");
+            else
+                Console.WriteLine("Given dates are equal");
+
+            //output-First date is greater
+
+            //Datetime now method gives the date of current time, here converted to the string
+            Console.WriteLine("Current DateTime :" + DateTime.Now.ToString());
+
+            //output-Current DateTime :16-10-2021 18:02:12
+
+            //To check if the dates are equal
+            bool objcompare = DateTime.Equals(objdate1, objdate2);
+
+            if (objcompare == true)
+                Console.WriteLine("Given dates are equal");
+            else
+                Console.WriteLine("Given dates are not equal");
+            //output-Given dates are not equal
 
 
 
 
-
-
-
-        //result - 	Day:17
-        //Month: 9
-        //Year: 2021
-        //Hour: 7
-        //Minute: 10
-        //Second: 24
-        //Millisecond: 0
-        //Day of Week: Friday
-        // Day of Year: 260
-        //Time of Day: 07:10:24
+            //result - 	Day:17
+            //Month: 9
+            //Year: 2021
+            //Hour: 7
+            //Minute: 10
+            //Second: 24
+            //Millisecond: 0
+            //Day of Week: Friday
+            // Day of Year: 260
+            //Time of Day: 07:10:24
 
         }
     }
--- module-2/various_methods_of_String_class/Program.cs
+++ module-2/various_methods_of_String_class/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace various_methods_of_String_class
 {
@@ -6,25 +7,48 @@ namespace various_methods_of_String_class
     {
         static void Main(string[] args)
         {
-            string a = "hello";
-            string b = "world";
-            string c = (String)a.Clone();
-            string d = string.Concat(a, b);
-            string e = string.Copy(d);
-            string f = a.Remove(2);
-            string g = b.Replace("wor","smit");
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(g);
+            string myName = "SmitVora";
+            //Clone() used to clone a string
+            string clonedOne = (String)myName.Clone();
+
+            //Equals() method used to check if both strings are equal in value
+            Console.WriteLine("Are both strings same : "+string.Equals(myName,clonedOne));
+            //output - Are both strings same : True
+
+            string hobby = " loves cricket";
+            //Concate() function to concatenate 2 strings
+            Console.WriteLine("Concatenated Strings : "+string.Concat(myName,hobby));
+            //output - Concatenated Strings : SmitVora loves cricket
+
+            //Contains() function to check existence of a substring
+            Console.WriteLine("Does string myName contains 'Smit': "+myName.Contains("Smit"));
+            //output - Does string myName contains 'Smit': True
+
+            //Remove() method to remove characters from a specified index
+            Console.WriteLine("Characters after Smit Removed and string becomes : "+myName.Remove(4));
+            //output - Characters after Smit Removed and string becomes : Smit
+
+            //Insert() method to insert in string in existing string from a certain index
+            Console.WriteLine("Use of insert to get new string : "+myName.Insert(8,hobby));
+            // output - Use of insert to get new string : SmitVora loves cricket
+
+            //Replace() Method to replace a certain part of a string with new string
+            Console.WriteLine("Replacing 'Vora' to 'likes finance':" +myName.Replace("Vora","loves finance"));
+            // output - Replacing 'Vora' to 'likes finance':Smitloves finance
+
+            //ToLower() method to lowercase all the letters in the string
+            Console.WriteLine("Lowercase String: "+myName.ToLower());
+            //output-Lowercase String: smitvora
+
+            //ToUpper() method to uppercase all the letters in the string
+            Console.WriteLine("Uppercase String: " + myName.ToUpper());
+            //output-Uppercase String: SMITVORA
 
 
-            //result – hello
-            //helloworld
-            //helloworld
-            //he
-            //smitld
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //using join method
+            Console.WriteLine("using join method : "+string.Join(",",numbers));
+            //output - using join method : 1,2,3,4,5,6,7,8,9
 
         }
     }
